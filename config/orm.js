b637a2843a67994f7b32d4216bda5c8d14f12312
Geddy Delonge <57271262+RyanEllingson@users.noreply.github.com>
2020-06-22 21:28:45 -0500

    wrote logic and validation for creating a new user, defined route for user registration

--- config/orm.js
+++ config/orm.js
@@ -1,4 +1,7 @@
 const connection = require("./connection");
+const Validator = require("validator");
+
+const validateRegisterInput = require("../validation/register");
 
 const orm = {
     findItem: function(req) {
@@ -15,8 +18,13 @@ const orm = {
         return new Promise(dbQuery);
     },
     findAndReturnItem: async function(req, res) {
-        const result = await orm.findItem(req);
-        res.json(result);
+        try {
+            const result = await orm.findItem(req);
+            res.json(result);
+        } catch (err) {
+            res.json({...err, error: true});
+        }
+        
     },
     getAllItems: function() {
         const queryString = "SELECT * FROM items";
@@ -31,8 +39,13 @@ const orm = {
         return new Promise(dbQuery);
     },
     getAndReturnAllItems: async function(req, res) {
-        const result = await orm.getAllItems();
-        res.json(result);
+        try {
+            const result = await orm.getAllItems();
+            res.json(result);
+        } catch (err) {
+            res.json({...err, error: true});
+        }
+        
     },
     getBotanyItems: function() {
         const queryString = "SELECT * FROM items WHERE discipline = 'botany'";
@@ -47,8 +60,13 @@ const orm = {
         return new Promise(dbQuery);
     },
     getAndReturnBotanyItems: async function(req, res) {
-        const result = await orm.getBotanyItems();
-        res.json(result);
+        try {
+            const result = await orm.getBotanyItems();
+            res.json(result);
+        } catch (err) {
+            res.json({...err, error: true});
+        }
+        
     },
     getMiningItems: function() {
         const queryString = "SELECT * FROM items WHERE discipline = 'mining'";
@@ -63,8 +81,39 @@ const orm = {
         return new Promise(dbQuery);
     },
     getAndReturnMiningItems: async function(req, res) {
-        const result = await orm.getMiningItems();
-        res.json(result);
+        try {
+            const result = await orm.getMiningItems();
+            res.json(result);
+        } catch(err) {
+            res.json({...err, error: true});
+        }
+        
+    },
+    registerUser: function(userInfo) {
+        const queryString = "INSERT INTO users SET ?";
+        const dbQuery = async function(resolve, reject) {
+            const {errors, isValid} = await validateRegisterInput(userInfo, connection);
+            if (!isValid) {
+                return reject(errors);
+            }
+            const normalizedEmail = Validator.normalizeEmail(userInfo.email);
+            connection.query(queryString, { email: normalizedEmail, password: userInfo.password }, function(err, result) {
+                if (err) {
+                    return reject(err);
+                }
+                return resolve(result);
+            });
+        };
+        return new Promise(dbQuery);
+    },
+    registerAndReturnUser: async function(req, res) {
+        try {
+            const result = await orm.registerUser(req.body);
+            res.json(result);
+        } catch (err) {
+            res.json({...err, error: true});
+        }
+        
     }
 };
 
