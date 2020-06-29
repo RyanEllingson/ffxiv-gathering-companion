47541397ec14fe9dfcd72e2e33d25c54155ad68d
Geddy Delonge <57271262+RyanEllingson@users.noreply.github.com>
2020-06-28 21:45:01 -0500

    added background image, added navbar and Home component

--- client/src/setupTests.js
+++ client/src/setupTests.js
@@ -2,4 +2,6 @@
 // allows you to do things like:
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
-import '@testing-library/jest-dom/extend-expect';
+
+// Uncomment this if you want to do front-end testing
+// import '@testing-library/jest-dom/extend-expect';
