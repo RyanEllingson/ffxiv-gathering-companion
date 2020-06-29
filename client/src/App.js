da7a827525ed704434e846399a1f4a69d4b55f23
Geddy Delonge <57271262+RyanEllingson@users.noreply.github.com>
2020-06-29 18:00:55 -0500

    started Items component, can get items from the database and log to the console

--- client/src/App.js
+++ client/src/App.js
@@ -6,36 +6,33 @@ import {
   NavLink
 } from "react-router-dom";
 import Home from "./components/Home";
+import Items from "./components/Items";
 
 function App() {
 
   return (
     <Router>
       <nav className="navbar navbar-expand-lg navbar-light bg-light mb-3">
-        <span className="navbar-brand">Navbar</span>
+        <span className="navbar-brand">FFXIV GC</span>
         <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
         <div className="collapse navbar-collapse" id="navbarNav">
           <ul className="navbar-nav ml-auto">
             <li className="nav-item">
-              <NavLink to="/" className="nav-link" activeClassName="active">Home</NavLink>
+              <NavLink to="/" exact className="nav-link" activeClassName="active">Home</NavLink>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="#">Features</a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#">Pricing</a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+              <NavLink to="/items" exact className="nav-link" activeClassName="active">Find Items</NavLink>
             </li>
           </ul>
         </div>
       </nav>
-      {/* <Navbar /> */}
       
       <Switch>
+        <Route path="/items">
+          <Items />
+        </Route>
         <Route path="/">
           <Home />
         </Route>
