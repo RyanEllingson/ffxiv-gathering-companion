26dd560e842d45f66eb365fa157343db5ba87cb1
Geddy Delonge <57271262+RyanEllingson@users.noreply.github.com>
2020-07-02 23:36:10 -0500

    refactor time code so it initializes with eorzea time rather than real world time

--- client/src/time/time.js
+++ client/src/time/time.js
@@ -4,20 +4,23 @@ const moment = require("moment");
 export const TimeContext = React.createContext("time");
 
 export function Time({ children }) {
-    const [time, setTime] = useState(moment());
+    const getEorzeaTime = function() {
+        const epochTime = moment().valueOf();
+        const eorzeaMultiplier = 3600/175;
+        const eorzeaMS = epochTime*eorzeaMultiplier;
+        const totalSeconds = eorzeaMS / 1000;
+        const totalMinutes = totalSeconds / 60;
+        const totalHours = totalMinutes / 60;
+        const hours = Math.floor(totalHours % 24);
+        const minutes = Math.floor(totalMinutes % 60);
+        return moment(`${hours}:${minutes}`, "H:mm");
+    };
+
+    const [time, setTime] = useState(getEorzeaTime());
 
     useEffect(() => {
         const myInterval = setInterval(function() {
-            const epochTime = moment().valueOf();
-            const eorzeaMultiplier = 3600/175;
-            const eorzeaMS = epochTime*eorzeaMultiplier;
-            const totalSeconds = eorzeaMS / 1000;
-            const totalMinutes = totalSeconds /60;
-            const totalHours = totalMinutes / 60;
-            const hours = Math.floor(totalHours % 24);
-            const minutes = Math.floor(totalMinutes % 60);
-            const eorzeaTime = moment(`${hours}:${minutes}`, "H:mm");
-            setTime(eorzeaTime);
+            setTime(getEorzeaTime());
         }, 2916);
 
         return function cleanup() {
