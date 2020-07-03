8ede9a278ef77abbebb1adf4ae91f651096ea6c4
Geddy Delonge <57271262+RyanEllingson@users.noreply.github.com>
2020-07-02 20:38:18 -0500

    login and register forms display errors sent back by API

--- client/src/components/Login/Login.js
+++ client/src/components/Login/Login.js
@@ -6,7 +6,7 @@ const axios = require("axios");
 const Login = function() {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
-    const [errors, setErrors] = useState(null);
+    const [errors, setErrors] = useState({});
     const { setUser } = useContext(AuthContext);
     const history = useHistory();
 
@@ -16,9 +16,10 @@ const Login = function() {
         .then(function(response) {
             console.log(response.data);
             if (response.data.error) {
-                
+                setErrors(response.data);
             } else {
                 setUser(response.data.email);
+                setErrors({});
                 history.push("/");
             }
         }).catch(function(error) {
@@ -37,11 +38,12 @@ const Login = function() {
                                 <div className="form-group">
                                     <label htmlFor="exampleInputEmail1">Email address</label>
                                     <input type="text" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" value={email} onChange={(e)=>setEmail(e.target.value)}/>
-                                    <small id="emailHelp" className="form-text text-muted">We'll never share your email with anyone else.</small>
+                                    {errors.email ? <small className="form-text text-muted">{errors.email}</small> : ""}
                                 </div>
                                 <div className="form-group">
                                     <label htmlFor="exampleInputPassword1">Password</label>
                                     <input type="password" className="form-control" id="exampleInputPassword1" value={password} onChange={(e)=>{setPassword(e.target.value)}}/>
+                                    {errors.password ? <small className="form-text text-muted">{errors.password}</small> : ""}
                                 </div>
                                 <button type="submit" className="btn btn-primary" onClick={(e)=>handleSubmit(e)}>Submit</button>
                             </form>
