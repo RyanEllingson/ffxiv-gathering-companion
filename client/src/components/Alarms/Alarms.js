e42a4830dae87fcbf92d7e0514643ede3026f640
Geddy Delonge <57271262+RyanEllingson@users.noreply.github.com>
2020-07-07 00:27:19 -0500

    now rendering currently-available alarms in a different UI element than not-yet-available alarms

--- client/src/components/Alarms/Alarms.js
+++ client/src/components/Alarms/Alarms.js
@@ -10,18 +10,18 @@ const Alarms = function() {
     const { user } = useContext(AuthContext);
     const { time } = useContext(TimeContext);
     const [alarms, setAlarms] = useState([]);
+    const [readyAlarms, setReadyAlarms] = useState([]);
     const history = useHistory();
 
     if (!user) {
         history.push("/");
     }
 
-    const sortedAlarms = function(alarmArray) {
+    const renderAlarms = function(alarmArray) {
         const newAlarms = [];
         const activeAlarms = [];
         const checkTime = moment(time.format("HH"), "HH");
         for (let i=0; i<24; i++) {
-            console.log(checkTime.format("HH"));
             for (let j=0; j<alarmArray.length; j++) {
                 if (checkTime.format("HH") === alarmArray[j].start_time.split(":")[0]) {
                     if (isActive(alarmArray[j].start_time, alarmArray[j].duration)) {
@@ -33,14 +33,15 @@ const Alarms = function() {
             }
             checkTime.add(1, "hours");
         }
-        return activeAlarms.concat(newAlarms);
+        setAlarms(newAlarms);
+        setReadyAlarms(activeAlarms);
     }
 
-    const renderAlarms = function() {
+    const fetchAlarms = function() {
         axios.get(`/api/alarms/${user}`)
         .then(function(response) {
             if (!response.data.error) {
-                setAlarms(sortedAlarms(response.data));
+                renderAlarms(response.data);
             }
         })
         .catch(function(err) {
@@ -50,16 +51,17 @@ const Alarms = function() {
 
     let delay = false;
 
-    if (time.get("minutes") === 0 && !delay) {
+    if (time.get("minutes") === 1 && !delay) {
         delay = true;
         setTimeout(() => {
             delay = false;
-            setAlarms(sortedAlarms(alarms));
+            const alarmSet = readyAlarms.concat(alarms);
+            renderAlarms(alarmSet);
         }, 3500);
     }
 
     useEffect(() => {
-        renderAlarms();
+        fetchAlarms();
     }, []);
 
 
@@ -67,7 +69,7 @@ const Alarms = function() {
         event.preventDefault();
         axios.delete(`/api/alarms/${id}`)
         .then(function(response) {
-            renderAlarms();
+            fetchAlarms();
         })
         .catch(function(error) {
             console.error(error);
@@ -86,6 +88,36 @@ const Alarms = function() {
             <div className="row">
                 <div className="col-12">
                     <div class="card mb-5">
+                        <h5 className="card-header text-center">Ready to Gather!</h5>
+                        <ul class="list-group">
+                            {readyAlarms.map((alarm) => {
+                                return <li className="list-group-item">
+                                    <ItemCard
+                                        key={alarm.id}
+                                        aetheryte={alarm.aetheryte}
+                                        coordinates={alarm.coordinates}
+                                        discipline={alarm.discipline}
+                                        duration={alarm.duration}
+                                        image={alarm.image_url}
+                                        name={alarm.item_name}
+                                        type={alarm.node_type}
+                                        region={alarm.region}
+                                        start={alarm.start_time}
+                                        notes={alarm.notes}
+                                    >
+                                        <div className="col-5 col-sm-4 col-md-3 text-right">
+                                            <button className="btn btn-danger" onClick={(e)=>{deleteAlarm(e, alarm.id)}}>Delete alarm</button>
+                                        </div>
+                                    </ItemCard>
+                                </li>
+                            })}
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div className="row">
+                <div className="col-12">
+                    <div class="card mb-5">
                         <ul class="list-group">
                             {alarms.map((alarm) => {
                                 return <li className="list-group-item">
@@ -101,7 +133,6 @@ const Alarms = function() {
                                         region={alarm.region}
                                         start={alarm.start_time}
                                         notes={alarm.notes}
-                                        active={isActive(alarm.start_time, alarm.duration)}
                                     >
                                         <div className="col-5 col-sm-4 col-md-3 text-right">
                                             <button className="btn btn-danger" onClick={(e)=>{deleteAlarm(e, alarm.id)}}>Delete alarm</button>
